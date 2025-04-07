c5cdbdd803eb5d4ef597c23470f83e4e78a56e4a
Choukette-offi <clementmoisan16@gamil.com>
2025-04-07 08:03:08 +0200

    creation du répertoire java

---
+++ Java/src/AppLivreExpress.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppLivreExpress {
+    boolean quitter;
+    
+    AppWeekEnd(WeekEnd we) {
+        this.we = we;
+        this.quitter = false;
+        this.personne_selectionnee = null;
+        }
+}
---
+++ Java/src/UIWeekEnd.java
@@ -0,0 +1,7 @@
+class UIWeekEnd {    
+    public static void main(String[] args) {
+	WeekEnd leWeekEnd = new WeekEnd();
+	AppWeekEnd app = new AppWeekEnd(leWeekEnd);
+	app.run();
+    }
+}
