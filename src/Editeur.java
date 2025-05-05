d28629aa97dfdc2559a3a8f45c0e204f0d2c3aa3
NolhanDolou <nolhan.dolou@etu.univ-orleans.fr>
2025-05-05 12:14:20 +0200

    push editeur

---
+++ src/Editeur.java
@@ -0,0 +1,13 @@
+public class Editeur {
+    private String nom;
+    
+    public Editeur(String nom) {
+        this.nom = nom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
