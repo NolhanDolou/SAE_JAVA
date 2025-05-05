58a87876cf9806d121c23e882fee9eb4535948a6
NolhanDolou <nolhan.dolou@etu.univ-orleans.fr>
2025-05-05 12:23:34 +0200

    client et personne modif

--- src/Personne.java
+++ src/Personne.java
@@ -1,16 +1,11 @@
 public abstract class Personne{
     private String nom;
     private String prenom;
-    private String adresse;
-    private int codePostal;
-    private String ville;
+    
 
-    public Personne(String nom, String prenom, String adresse, int codePostal, String ville){
+    public Personne(String nom, String prenom){
         this.nom = nom;
         this.prenom = prenom;
-        this.adresse  = adresse;
-        this.codePostal = codePostal;
-        this.ville = ville;
     }
 
     public String getNom() {
@@ -19,18 +14,6 @@ public abstract class Personne{
 
     public String getPrenom() {
         return prenom;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public int getCodePostal() {
-        return codePostal;
-    }
-
-    public String getVille() {
-        return ville;
     }
 
 }
