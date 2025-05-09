071f0839f89edf70041e1b71ba792a4bb7dc32f1
tifanymeunier <tifany.meunier@etu.univ-orleans.fr>
2025-05-09 11:38:37 +0200

    Add files via upload

--- src/Vendeur.java
+++ src/Vendeur.java
@@ -4,15 +4,40 @@ public class Vendeur extends Personne {
 
     private Magasin magasin;
 
-    public Vendeur(String nom, String prenom, String adresse, int codePostal, String ville, Magasin magasin){
+    public Vendeur(String nom, String prenom){
         super(nom, prenom, adresse, codePostal, ville);
         this.magasin=magasin;
     }
 
     public void ajouterLivre(String titre, Auteur auteur, int nbDePages, double prix, List<Classification> themes){
         Livre livre = new Livre(titre, auteur, nbDePages, prix, themes);
-        magasin.stock.add(livre);
+        this.magasin.stock.add(livre);
     }
 
-    
+    public void majStock(Livre livre, int qte){
+        if(qte>0){
+            for(int i=0;i<qte;i++){
+                this.ajouterLivre(livre.titre, livre.auteur, livre.nbDePages, livre.prix, livre.themes);
+            }}
+        else{
+            for(int i=0;i>qte;i--){
+                this.magasin.stock.remove(livre);}
+        }
+    } 
+
+    public boolean estDispo(Livre livre, Magasin magasin){
+        if(magasin.stock.contains(livre)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean commandeClient(Livre livre, Client client, int qte){
+        return client.commanderLivre(livre, qte);
+    }
+
+    public boolean transfertLivre(Livre livre, Magasin autreMagasin){
+        autreMagasin.stock.remove(livre);
+        this.majStock(livre, this.magasin);
+    }
 }
