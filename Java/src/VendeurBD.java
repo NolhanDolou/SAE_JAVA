5b4986288e053cd7ace38c344bc84f760e891aef
ClementMoisan <clementmoisan16@gmail.com>
2025-06-11 08:29:27 +0200

    vendeurDB

--- Java/src/VendeurBD.java
+++ Java/src/VendeurBD.java
@@ -8,25 +8,7 @@ public class VendeurBD {
 	}
 
 	public String modifierLivre(String isbn, String idmag, Integer qte ){
-		String requete = "UPDATE livre SET prix = 20 WHERE id = 1";
-		try {
-			st = laConnexion.getConnection().createStatement();
-			int rowsAffected = st.executeUpdate(requete);
-			if (rowsAffected > 0) {
-				return "Livre modifié avec succès.";
-			} else {
-				return "Aucun livre trouvé avec cet ID.";
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return "Erreur lors de la modification du livre.";
-		} finally {
-			try {
-				if (st != null) st.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 
 }
