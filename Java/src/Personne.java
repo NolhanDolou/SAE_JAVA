ba4eab69b01d6aff65ca33323d64e330c2b016fd
Flaviobtn <flavio.bouton@etu.univ-orleans.fr>
2025-06-14 16:57:52 +0200

    Organisation arborescence

--- Java/src/Personne.java
+++
@@ -1,8 +0,0 @@
-class Personne{
-    private String nom;
-    private String prenom;
-    public Personne( String n, String p) {this.nom = n; this.prenom = p;}
-    public String toString() {return this.prenom + " " + this.nom + " ";  }
-    public String getNom() {return this.nom;}
-    public String getPrenom() {return this.prenom;}
-}
