2dffb35d96172734068cc2c30f4f8842e8b4138f
Flaviobtn <flavio.bouton@etu.univ-orleans.fr>
2025-06-14 18:12:31 +0200

    Co-authored-by: ClementM37 <clement.moisan@etu.univ-orleans.fr>

--- code/lancer_linux.sh
+++ code/lancer_linux.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-javac -d ./bin ./src/ConnexionBD.java
-java -cp ./bin ConnexionBD
+# Compiler le fichier Java avec le .jar
+javac -cp "./lib/mariadb-java-client-3.3.2.jar" -d bin src/ConnexionBD.java
+
+# Lancer le programme
+java -cp "bin:lib/mariadb-java-client-3.3.2.jar" ConnexionBD
--- code/lancer_windows.bat
+++ code/lancer_windows.bat
@@ -1,2 +1,2 @@
-javac --release 8 -d ./bin ./src/ConnexionBD.java
-java -cp ./bin ConnexionBD
+javac --release 8 -cp "lib/mariadb-java-client-3.5.3.jar" -d bin src/ConnexionBD.java
+java -cp "bin;lib/mariadb-java-client-3.5.3.jar" ConnexionBD
